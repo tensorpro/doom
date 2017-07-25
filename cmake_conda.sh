f56b91cc73cf0923f3a95145e0c6e61403d25327
Akshay Mishra <lck.akshay@gmail.com>
2017-07-25 18:55:29 -0400

    Made cmake_conda general

--- cmake_conda.sh
+++ cmake_conda.sh
@@ -1,2 +1,2 @@
 #!/bin/bash	
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_PYTHON=ON -DPYTHON_INCLUDE_DIR=$HOME/.miniconda2/include/python2.7 -DPYTHON_LIBRARY=$HOME/.miniconda2/lib/libpython2.7.so  -DPYTHON_EXECUTABLE=$HOME/.miniconda2/bin/python2.7 -DNUMPY_INCLUDES=$HOME/.miniconda2/lib/python2.7/site-packages/numpy/core/include
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_PYTHON=ON -DPYTHON_INCLUDE_DIR=$HOME/$1/include/python2.7 -DPYTHON_LIBRARY=$HOME/$1/lib/libpython2.7.so  -DPYTHON_EXECUTABLE=$HOME/$1/bin/python2.7 -DNUMPY_INCLUDES=$HOME/$1/lib/python2.7/site-packages/numpy/core/include
