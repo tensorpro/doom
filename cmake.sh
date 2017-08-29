98c736a560e129d2175434db2a08fa44ad48f2ba
shotaroikeda <ikeda.shot@gmail.com>
2017-08-29 00:15:23 -0500

    make cmake support osx

--- cmake.sh
+++ cmake.sh
@@ -1,14 +1,33 @@
 #!/bin/bash	
-BASEDIR=$HOME/anaconda2
 
-echo $BASEDIR
+BASEDIR=$CONDA_PREFIX
 
-./cmake_clean.sh
-rm -rf ./bin
+# Detect operating system
+if [[ "$(uname)" == "Darwin" ]]; then
+        echo "Using OSX Settings for compilation"
+        if [[ $BASEDIR == "" ]]; then
+                BASEDIR=$HOME/.anaconda
+        fi
+        echo $BASEDIR
 
-cmake . -DCMAKE_BUILD_TYPE=Release -DBUILD_PYTHON=ON \
+        F_LIB="dylib"
+else
+        echo "Using Linux Settings for compilation"
+        if [[ $BASEDIR == "" ]]; then
+                BASEDIR=$HOME/anaconda2
+        fi
+
+        F_LIB="so"
+fi
+
+echo $BASEDIR/lib/libpython2.7.$F_LIB
+
+# ./cmake_clean.sh
+# rm -rf ./bin
+
+cmake . -DCMAKE_BUILD_TYPE=Debug -DBUILD_PYTHON=ON \
 -DPYTHON_INCLUDE_DIR=$BASEDIR/include/python2.7 \
--DPYTHON_LIBRARY=$BASEDIR/lib/libpython2.7.so \
+-DPYTHON_LIBRARY=$BASEDIR/lib/libpython2.7.$F_LIB \
 -DPYTHON_EXECUTABLE=$BASEDIR/bin/python2.7 \
 -DNUMPY_INCLUDES=$BASEDIR/lib/python2.7/site-packages/numpy/core/include
 
@@ -16,4 +35,4 @@ make -j
 
 rm -rf $BASEDIR/lib/python2.7/site-packages/vizdoom
 mkdir -p $BASEDIR/lib/python2.7/site-packages/vizdoom
-cp -r bin/python2/pip_package/ $BASEDIR/lib/python2.7/site-pcakages/vizdoom
+cp -r bin/python2/pip_package/ $BASEDIR/lib/python2.7/site-packages/vizdoom
